4beed5f52818199e87c5a754f07c89f6e0e4b5aa
Julio Guedes <juliooguedes@gmail.com>
2025-01-16 17:48:38 -0300

    Atualização v2

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function closeMenu() {
-    document.getElementById('toggle').checked = false;
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+  toggleMenu();
+})
+
+
+function toggleMenu() {
+  const nav = document.querySelector('.nav-resposive');
+  menuHamburguer.classList.toggle('change')
+
+  if(menuHamburguer.classList.contains('change')) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
   }
-  
+}
